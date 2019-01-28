f617acda8a78bd92dcac155fea64b7fc8b46d9de
Swayam Joshi <swayam@usf.edu>
2019-01-27 23:43:45 -0500

    A1

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,12 +1,253 @@
 ﻿using System;
+using System.Diagnostics;
 
-namespace ConsoleApp6
+
+namespace Assignment1_S19
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int a = 5, b = 15;
+            printPrimeNumbers(a, b);
+
+
+            int n1 = 5;
+            double r1 = getSeriesResult(n1);
+            Console.WriteLine("The sum of the series is: " + r1);
+
+
+            long n2 = 15;
+            long r2 = decimalToBinary(n2);
+            Console.WriteLine("Binary conversion of the decimal number " + n2 + " is: " + r2);
+
+
+            long n3 = 1111;
+            long r3 = binaryToDecimal(n3);
+            Console.WriteLine("Decimal conversion of the binary number " + n3 + " is: " + r3);
+
+
+            int n4 = 5;
+            printTriangle(n4);
+
+            int[] arr = new int[] { 1, 2, 3, 2, 2, 1, 3, 2 };
+            computeFrequency(arr);
+            Console.ReadKey();
+            // write your self-reflection here as a comment
+
+        }
+
+
+
+        //modified
+        public static bool isPrime(int q)
+        {
+            bool isPrime = false;
+            for (int i = 2; i < q; i++)
+            {
+                if (q % i == 0)
+                {
+                    isPrime = false;
+                    break;
+
+                }
+                else
+                {
+                    isPrime = true;
+
+                }
+            }
+            return isPrime;
+        }
+        public static void printPrimeNumbers(int x, int y)
+        {
+            try
+            {
+                for (int i = x; i <= y; i++)
+                {
+
+                    if (isPrime(i) == true)
+                    {
+                        Console.WriteLine(+i + "\t");
+                    }
+
+                }
+                //Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printPrimeNumbers()");
+            }
+
+        }
+
+
+        public static double factorial(int i)
+        {
+            int a = 1;
+            for (int n = 1; n <= i; n++)
+            {
+                a *= n;
+            }
+            return a;
+        }
+        public static double getSeriesResult(int n)
+        {
+            try
+            {
+                double a = 0;
+                double b = 0;
+                double prod1 = 0;
+                double prod2 = 0;
+
+
+                for (int p = 1; p <= n; p += 2)
+                {
+                    prod1 = factorial(p) / (p + 1);
+                    a += prod1;
+
+                }
+                for (int p = 2; p <= n; p += 2)
+                {
+                    prod2 = factorial(p) / (p + 1);
+                    b += prod2;
+
+                }
+
+                return Math.Round(a - b, 3);
+
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing getSeriesResult()");
+            }
+
+            return 0;
+        }
+
+        public static long decimalToBinary(long n)
+        {
+            try
+            {
+                long rem = 0;
+                long divd = n;
+                String str = "";
+                for (; divd > 0;)
+                {
+                    rem = divd % 2;
+                    str += rem;
+                    divd /= 2;
+                }
+                return Convert.ToInt64(str);
+
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing decimalToBinary()");
+            }
+
+            return 0;
+        }
+
+        public static long binaryToDecimal(long n)
+        {
+
+
+            try
+            {
+                String str = n.ToString();
+                int a = 1;
+                int b = 0;
+                for (int p = str.Length - 1; p >= 0; p--)
+                {
+                    if (str[p] == '1')
+                    {
+                        b += a;
+                    }
+                    a *= 2;
+                }
+                return (Convert.ToInt64(b));
+
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing binaryToDecimal()");
+            }
+
+            return 0;
+        }
+
+        public static void printTriangle(int n)
+        {
+            try
+            {
+                for (int a = 1; a <= n; a++)
+                {
+                    for (int b = n; b >= a; b--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int c = 1; c <= a; c++)
+                    {
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printTriangle()");
+            }
+        }
+
+        public static void computeFrequency(int[] a)
+        {
+            try
+            {
+                int count;
+                int arLen = a.Length;
+                int[] rep = new int[arLen];
+                for (int p = 0; p < arLen; p++)
+                {
+                    rep[p] = -1;
+                }
+
+                for (int p = 0; p < arLen; p++)
+                {
+                    count = 1;
+                    for (int q = p + 1; q < arLen; q++)
+                    {
+                        if (a[p] == a[q])
+                        {
+                            count++;
+                            rep[q] = 0;
+                        }
+                    }
+                    if (rep[p] != 0) ;
+                    {
+                        rep[p] = count;
+                    }
+                }
+
+                Console.Write("Frequency of element: \n");
+                Console.Write("Number \t Frequency \n");
+                for (int p = 0; p < arLen; p++)
+                {
+                    if (rep[p] != 0)
+                    {
+                        Console.Write(a[p] + "\t" + rep[p] + "\n");
+                    }
+                }
+                Console.ReadKey();
+
+            }
+
+            catch
+            {
+                Console.WriteLine("Exception occured while computing computeFrequency()");
+            }
         }
     }
 }
+
